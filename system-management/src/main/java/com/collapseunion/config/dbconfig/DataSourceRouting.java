0dd1b8b25f65d66e70c13aa09214c3f9a3f83037
CloudSen <www.402645063@gmail.com>
2019-06-13 18:34:47 +0800

    feat(system-management, springConfig): add druid dynamic data source

---
+++ system-management/src/main/java/com/collapseunion/config/dbconfig/DataSourceRouting.java
@@ -0,0 +1,34 @@
+package com.collapseunion.config.dbconfig;
+
+import com.collapseunion.util.ThreadLocalUtil;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * 动态切换数据源
+ *
+ * @author CloudSen
+ */
+public class DataSourceRouting extends AbstractRoutingDataSource {
+
+    public enum DbKey {
+        /**
+         * 开发库
+         */
+        DEV,
+        /**
+         * 测试库
+         */
+        TEST,
+        /**
+         * 线上库
+         */
+        PROD,
+        ;
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return ThreadLocalUtil.getDbKey();
+    }
+}
+
