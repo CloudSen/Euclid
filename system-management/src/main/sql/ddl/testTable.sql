d663e76fcd4d1afa84e75e3a07e47b6309501656
CloudSen <www.402645063@gmail.com>
2019-06-14 00:40:26 +0800

    doc(system-management, sql): add PostgreSQL ddl files

---
+++ system-management/src/main/sql/ddl/testTable.sql
@@ -0,0 +1,28 @@
+CREATE TABLE public.test_jpa
+(
+    id          character(36) NOT NULL,
+    name        character varying(20),
+    create_date date          NOT NULL,
+    update_date date          NOT NULL,
+    PRIMARY KEY (id)
+)
+    WITH (
+        OIDS = FALSE
+    );
+
+ALTER TABLE public.test_jpa
+    OWNER to postgres;
+COMMENT ON TABLE public.test_jpa
+    IS 'table for test jpa';
+
+COMMENT ON COLUMN public.test_jpa.id
+    IS 'UUID';
+
+COMMENT ON COLUMN public.test_jpa.name
+    IS '姓名';
+
+COMMENT ON COLUMN public.test_jpa.create_time
+    IS '创建时间';
+
+COMMENT ON COLUMN public.test_jpa.update_time
+    IS '更新时间';
