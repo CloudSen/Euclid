c870c2491202cc5215e2c5e0619e3a9a689bb214
CloudSen <www.402645063@gmail.com>
2019-07-13 18:26:10 +0800

    feat(zuul, java): add spring cloud config to zuul proxy server

--- zuul/src/main/java/com/collapseunion/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/collapseunion/zuul/ZuulApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * API路由
@@ -14,7 +14,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulServer;
  */
 @Slf4j
 @EnableEurekaClient
-@EnableZuulServer
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
     static {
